2b32bcaf8605ff6ea30e20854d7dfd733dfa4050
WasabyDev <wasabygay@gmail.com>
2023-05-05 04:55:39 +0900

    ss

--- www/js/script.js
+++ www/js/script.js
@@ -1,26 +1,18 @@
-function teste(){
-    alert("hello world")
-}
-
-localStorage.setItem("ADTO-1233", "Bruno")
-localStorage.setItem("KIOQ-O123", "soul")
-localStorage.setItem("LEOÇ-2415", "Felipe Neto")
-localStorage.setItem("LWSO-4795", "Diogo Defante")
-localStorage.setItem("SDLA-1233", "dela")
-
-function exibir(){
-
-    var exibir
-    var digitado
-
-    digitado =  document.getElementById('coletortxt').value
-    exibir = localStorage.getItem(digitado)
-    alert(exibir)
-}
-
 function cadastrar(){
-    placa = document.getElementById('placa').value
-    proprietario = document.getElementById('proprietario').value
 
-    localStorage.setItem(placa, proprietario)
+    emaild = document.getElementById('email').value
+    senha = document.getElementById('senha').value
+    alert('cadastro feito com sucesso')
+    localStorage.setItem(email, senha)
+
+    emaild =  localStorage.getItem(email)
+}
+function logar(){
+    
+    if(email == email && senha == senha){
+        alert('bem vindo')
+    }else{
+        alert('Usuario ou Senha incorretos')
+    }
+
 }
